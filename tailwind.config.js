408c5f7898fef9073457735cbe71d320efc0addd
Gabriel Rodrigues <jipacoding@gmail.com>
2021-01-03 07:15:43 -0400

    feat: setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  prefix: "tw-",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
