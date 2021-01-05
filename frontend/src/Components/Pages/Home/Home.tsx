6bb7126141c60a3a9a36e7c2485f0a1d3032ae85
Gabriel Rodrigues <jipacoding@gmail.com>
2021-01-05 15:22:37 -0400

    feat(frontend): Home: popup: Configurações

--- frontend/src/Components/Pages/Home/Home.tsx
+++ frontend/src/Components/Pages/Home/Home.tsx
@@ -1,20 +1,29 @@
 import {
-  IonContent,
-  IonHeader,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonItem,
+  IonList,
   IonPage,
-  IonTitle,
-  IonToolbar,
+  IonPopover,
+  IonRefresher,
+  IonRefresherContent,
   useIonViewWillEnter,
 } from "@ionic/react";
 import { Api, ITrain } from "@ya-time-marker/lib";
+import { ellipsisHorizontal, ellipsisVertical, search } from "ionicons/icons";
 import React, { Fragment, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { ROUTE_SEARCH, ROUTE_SETTINGS } from "../../Routes";
 import TrainCard from "../../TrainCard";
+import { HomeHeaderLayout } from "./HomeHeaderLayout";
 
 const {
   Trains: { getTrains },
 } = Api;
 
 const Home: React.FC = () => {
+  const history = useHistory();
   const [trains, setTrains] = useState<ITrain[]>([]);
 
   async function fetchTrains() {
@@ -29,14 +38,92 @@ const Home: React.FC = () => {
     fetchTrains();
   }, []);
 
+  const [popoverState, setShowPopover] = useState({
+    showPopover: false,
+    event: undefined,
+  });
+
+  function displayPopOver(event: any) {
+    setShowPopover(
+      event
+        ? { showPopover: true, event }
+        : { showPopover: false, event: undefined },
+    );
+  }
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Início</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
+      <HomeHeaderLayout
+        toolbarTitle="Início"
+        toolbarActions={
+          <IonButtons slot="end">
+            <IonButton
+              onClick={() => history.push(ROUTE_SEARCH())}
+              children={<IonIcon slot="icon-only" icon={search} />}
+            />
+            <IonButton
+              onClick={(event) => {
+                event.persist();
+                displayPopOver(event);
+              }}
+              children={
+                <IonIcon
+                  slot="icon-only"
+                  md={ellipsisVertical}
+                  ios={ellipsisHorizontal}
+                />
+              }
+            />
+          </IonButtons>
+        }
+      >
+        <IonRefresher
+          slot="fixed"
+          onIonRefresh={async (e) => {
+            try {
+              await fetchTrains();
+            } catch (_) {}
+            (e.target as HTMLIonRefresherElement).complete();
+          }}
+        >
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+        <>
+          <div>
+            <IonPopover
+              translucent
+              event={popoverState.event}
+              isOpen={popoverState.showPopover}
+              onDidDismiss={() => displayPopOver(undefined)}
+            >
+              <IonList>
+                {([
+                  {
+                    text: "Configurações",
+                    action: () => history.push(ROUTE_SETTINGS()),
+                  },
+                ] as {
+                  text: string;
+                  action: () => any;
+                  hidePopOver?: boolean;
+                }[]).map(({ text, action }, idx, { length }) => (
+                  <Fragment key={idx}>
+                    <IonItem
+                      button
+                      detail={false}
+                      children={text}
+                      onClick={() => {
+                        displayPopOver(undefined);
+                        action();
+                      }}
+                      {...(idx === length - 1 ? { lines: "none" } : {})}
+                    />
+                  </Fragment>
+                ))}
+              </IonList>
+            </IonPopover>
+          </div>
+        </>
         <div>
           {trains.map((train, idx) => (
             <Fragment
@@ -45,7 +132,7 @@ const Home: React.FC = () => {
             />
           ))}
         </div>
-      </IonContent>
+      </HomeHeaderLayout>
     </IonPage>
   );
 };
---
+++ frontend/src/Components/Pages/Home/HomeHeaderLayout.tsx
@@ -0,0 +1,50 @@
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  isPlatform,
+} from "@ionic/react";
+import React from "react";
+
+export type HomeHeaderLayoutProps = {
+  toolbarActions: any;
+  toolbarTitle: string;
+};
+
+export const HomeHeaderLayout: React.FC<HomeHeaderLayoutProps> = ({
+  children,
+  toolbarTitle,
+  toolbarActions,
+}) => {
+  const isIOS = isPlatform("ios");
+  return (
+    <>
+      {!isIOS && (
+        <IonHeader translucent>
+          <IonToolbar>
+            <IonTitle>{toolbarTitle}</IonTitle>
+            {toolbarActions}
+          </IonToolbar>
+        </IonHeader>
+      )}
+      <IonContent fullscreen={isIOS}>
+        {isIOS && (
+          <IonHeader collapse="condense">
+            <IonToolbar className="tw-flex tw-items-center tw-flex-wrap tw-pb-2">
+              <IonTitle
+                className="tw-flex tw-relative tw-min-w-min tw-pb-0 tw-leading-none tw-mt-1"
+                size="large"
+                slot="start"
+              >
+                {toolbarTitle}
+              </IonTitle>
+              {toolbarActions}
+            </IonToolbar>
+          </IonHeader>
+        )}
+        {children}
+      </IonContent>
+    </>
+  );
+};
