9bdc93d250aa9e925a8777c59edb33fa1028b955
Gabriel Rodrigues <jipacoding@gmail.com>
2021-01-08 12:46:55 -0400

    change(frontend): App: move Page imports to AppPages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,16 @@ import { IonApp, IonRouterOutlet } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import "@ionic/react/css/core.css";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import React, { lazy, Suspense } from "react";
+import React, { Suspense } from "react";
 import { Redirect, Route } from "react-router-dom";
 import "./App.css";
+import {
+  PageHome,
+  PageNewTrain,
+  PageProfile,
+  PageSearch,
+  PageSettings,
+} from "./AppPages";
 import "./Assets/tw-dist.css";
 import { useAppTheme } from "./Components/Hooks/useAppTheme";
 import LoadingPage from "./Components/LoadingPage";
@@ -16,20 +23,6 @@ import {
   ROUTE_TRAIN_NEW,
 } from "./Components/Routes";
 import "./theme/variables.css";
-
-const Promise_PageHome = import("./Components/Pages/Home/Home");
-const Promise_PageSearch = import("./Components/Pages/Search/Search");
-const Promise_PageProfile = import("./Components/Pages/Profile/Profile");
-const Promise_PageSettings = import("./Components/Pages/Settings/Settings");
-const Promise_PageNewTrain = import(
-  "./Components/Pages/Train/NewTrain/NewTrain"
-);
-
-const PageHome = lazy(() => Promise_PageHome);
-const PageSearch = lazy(() => Promise_PageSearch);
-const PageProfile = lazy(() => Promise_PageProfile);
-const PageSettings = lazy(() => Promise_PageSettings);
-const PageNewTrain = lazy(() => Promise_PageNewTrain);
 
 const App: React.FC = () => {
   const [theme] = useAppTheme();
---
+++ frontend/src/AppPages.tsx
@@ -0,0 +1,15 @@
+import { lazy } from "react";
+
+const Promise_PageHome = import("./Components/Pages/Home/Home");
+const Promise_PageSearch = import("./Components/Pages/Search/Search");
+const Promise_PageProfile = import("./Components/Pages/Profile/Profile");
+const Promise_PageSettings = import("./Components/Pages/Settings/Settings");
+const Promise_PageNewTrain = import(
+  "./Components/Pages/Train/NewTrain/NewTrain"
+);
+
+export const PageHome = lazy(() => Promise_PageHome);
+export const PageSearch = lazy(() => Promise_PageSearch);
+export const PageProfile = lazy(() => Promise_PageProfile);
+export const PageSettings = lazy(() => Promise_PageSettings);
+export const PageNewTrain = lazy(() => Promise_PageNewTrain);
