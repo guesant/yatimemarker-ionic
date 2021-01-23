b68103a7086f24748d6ad61a3b9117ec3c6e4954
Gabriel Rodrigues <jipacoding@gmail.com>
2021-01-23 14:59:57 -0400

    docs: apply license

--- ya-time-marker-lib/src/utils/__test__/renderTemplate.test.ts
+++ ya-time-marker-lib/src/utils/__test__/renderTemplate.test.ts
@@ -1,3 +1,10 @@
+//region Preamble
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright © 2021 Gabriel Rodrigues
+ */
+//endregion
+
 import { renderTemplate } from "../renderTemplate";
 
 test("renderTemplate", () => {
--- ya-time-marker-lib/src/utils/renderTemplate.ts
+++ ya-time-marker-lib/src/utils/renderTemplate.ts
@@ -1,3 +1,10 @@
+//region Preamble
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright © 2021 Gabriel Rodrigues
+ */
+//endregion
+
 import nunjucks from "nunjucks";
 
 export const renderTemplate = <CTX extends object = any>(template: string) => (
