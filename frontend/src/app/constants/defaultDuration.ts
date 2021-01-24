f7c5bcd9cb49dcd78d6cc6eeafcc9f52678073d7
Gabriel Rodrigues <jipacoding@gmail.com>
2021-01-24 09:01:36 -0400

    docs: apply license

--- frontend/src/app/constants/defaultDuration.ts
+++ frontend/src/app/constants/defaultDuration.ts
@@ -1,3 +1,10 @@
+//region Preamble
+/**
+ * SPDX-License-Identifier: MIT
+ * Copyright © 2021 Gabriel Rodrigues
+ */
+//endregion
+
 export const DEFAULT_TRAIN_DURATION = "30s";
 export const DEFAULT_INTERVAL_DURATION = "10s";
 export const DEFAULT_START_COUNTDOWN_DURATION = "15s";
