bbb441c4266053e60dd3e607d6c3a97a5b88cb68
Gabriel Rodrigues <jipacoding@gmail.com>
2021-02-01 15:02:40 -0400

    feat(frontend): Train: ViewTrain: show steps duration

--- frontend/src/app/components/Pages/Train/ViewTrain/ViewTrain.tsx
+++ frontend/src/app/components/Pages/Train/ViewTrain/ViewTrain.tsx
@@ -12,6 +12,7 @@ import {
   IonContent,
   IonHeader,
   IonItem,
+  IonLabel,
   IonList,
   IonPage,
   IonTitle,
@@ -23,11 +24,18 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { ITrain } from "@ya-time-marker/lib";
 import { deleteTrain } from "@ya-time-marker/lib/build/Api/Trains/deleteTrain";
 import { getTrain } from "@ya-time-marker/lib/build/Api/Trains/getTrain";
+import { computeAllTrainDurations } from "@ya-time-marker/lib/build/utils/computeAllTrainDurations";
+import { computeStepDuration } from "@ya-time-marker/lib/build/utils/computeStepDuration";
 import { close, trash } from "ionicons/icons";
+import ms from "ms";
 import React, { Fragment, useCallback, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import { useFetch } from "../../../Hooks/useFetch";
 import { ROUTE_HOME, ROUTE_TRAIN_START } from "../../../Routes";
+import { useStepContext } from "../Hooks/useStepContext";
+
+const extendedStringifyMS = (duration: number) =>
+  duration === Infinity ? String(Infinity) : ms(duration);
 
 const ViewTrain: React.FC = () => {
   const history = useHistory();
@@ -46,6 +54,8 @@ const ViewTrain: React.FC = () => {
     await deleteTrain(id);
     history.push(ROUTE_HOME());
   }, [history, id]);
+
+  const stepContext = useStepContext();
 
   return (
     <IonPage>
@@ -76,52 +86,96 @@ const ViewTrain: React.FC = () => {
             </div>
           </>
         )}
-        {!isLoading && train && (
-          <>
-            <IonActionSheet
-              isOpen={showActionSheet}
-              onDidDismiss={() => setShowActionSheet(false)}
-              buttons={[
-                {
-                  text: "Deletar Treino",
-                  role: "destructive",
-                  icon: trash,
-                  handler: actionDeleteTrain,
-                },
-                {
-                  text: "Cancelar",
-                  icon: close,
-                  role: "cancel",
-                },
-              ]}
-            />
-            <div className="tw-flex tw-flex-col tw-h-full">
-              <div>
-                <div className="tw-px-4 tw-my-4">
-                  <IonList>
-                    {train.steps.map(({ meta: { description } }, idx) => (
-                      <Fragment key={idx}>
-                        <IonItem button={false} detail={false}>
-                          {description}
+        {!isLoading &&
+          train &&
+          (() => {
+            const { steps } = train;
+            return (
+              <>
+                <IonActionSheet
+                  isOpen={showActionSheet}
+                  onDidDismiss={() => setShowActionSheet(false)}
+                  buttons={[
+                    {
+                      text: "Deletar Treino",
+                      role: "destructive",
+                      icon: trash,
+                      handler: actionDeleteTrain,
+                    },
+                    {
+                      text: "Cancelar",
+                      icon: close,
+                      role: "cancel",
+                    },
+                  ]}
+                />
+                <div className="tw-flex tw-flex-col tw-h-full">
+                  <div className="tw-flex-1 tw-overflow-y-auto">
+                    <div className="tw-px-4 tw-my-4">
+                      <IonList>
+                        {steps.map((step, idx) => {
+                          const { meta: { description = "" } = {} } = step;
+                          return (
+                            <Fragment key={idx}>
+                              <IonItem button={false} detail={false}>
+                                <IonLabel>{description}</IonLabel>
+                                <IonLabel slot="end" className="tw-text-right">
+                                  {extendedStringifyMS(
+                                    computeStepDuration(stepContext)(step),
+                                  )}
+                                </IonLabel>
+                              </IonItem>
+                            </Fragment>
+                          );
+                        })}
+                        {steps.length > 1 && (
+                          <IonItem>
+                            <IonLabel>{">"} Intervalo</IonLabel>
+                            <IonLabel slot="end" className="tw-text-right">
+                              {ms(stepContext.duration.interval)}
+                            </IonLabel>
+                          </IonItem>
+                        )}
+                        {steps.length > 0 && (
+                          <IonItem>
+                            <IonLabel>{">"} Contagem Regressiva</IonLabel>
+                            <IonLabel slot="end" className="tw-text-right">
+                              {ms(stepContext.duration.startCountdown)}
+                            </IonLabel>
+                          </IonItem>
+                        )}
+                      </IonList>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div>
+                      <IonList>
+                        <IonItem>
+                          <IonLabel>Total</IonLabel>
+                          <IonLabel slot="end" className="tw-text-right">
+                            {ms(
+                              computeAllTrainDurations(stepContext)(train)
+                                .totalDuration,
+                            )}
+                          </IonLabel>
                         </IonItem>
-                      </Fragment>
-                    ))}
-                  </IonList>
+                      </IonList>
+                    </div>
+
+                    <div className="tw-px-4 tw-py-3">
+                      <IonButton
+                        expand="block"
+                        onClick={() => history.push(ROUTE_TRAIN_START({ id }))}
+                      >
+                        Iniciar Treino
+                      </IonButton>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="tw-px-4">
-                <IonButton
-                  expand="block"
-                  onClick={() => {
-                    history.push(ROUTE_TRAIN_START({ id }));
-                  }}
-                >
-                  Iniciar Treino
-                </IonButton>
-              </div>
-            </div>
-          </>
-        )}
+              </>
+            );
+          })()}
       </IonContent>
     </IonPage>
   );
