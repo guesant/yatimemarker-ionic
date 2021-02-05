8822c988222dcb8ec9f716bd3495996311079477
Gabriel Rodrigues <jipacoding@gmail.com>
2021-02-05 19:55:25 -0400

    change(lib): Services: Database: simplify db instantiation

--- ya-time-marker-lib/src/Services/Database.ts
+++ ya-time-marker-lib/src/Services/Database.ts
@@ -9,13 +9,17 @@ const builtDBS = new Map<string, PouchDB.Database>();
 export class Database {
   static async createDB(dbName: string) {
     if (builtDBS.has(dbName)) return builtDBS.get(dbName)!;
-    let db: PouchDB.Database | null = null;
-    if (await verifyCanPerformIndexedDB()) {
-      db = new PouchDB(dbName);
-      builtDBS.set(dbName, db);
-    } else {
-      db = new PouchDB(dbName, { adapter: "memory" });
-    }
+
+    const db = await (async (): Promise<PouchDB.Database | null> => {
+      if (await verifyCanPerformIndexedDB()) {
+        const db = new PouchDB(dbName);
+        builtDBS.set(dbName, db);
+        return db;
+      } else {
+        return new PouchDB(dbName, { adapter: "memory" });
+      }
+    })();
+
     return db!;
   }
 }
