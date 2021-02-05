e1132ce8525c196d1e0a8a58e9cdb5976984796d
Gabriel Rodrigues <jipacoding@gmail.com>
2021-02-05 11:12:01 -0400

    refactor(frontend): AppPages

--- frontend/src/AppPages.tsx
+++ frontend/src/AppPages.tsx
@@ -11,42 +11,40 @@ const BASE_PAGES_PATH = "./app/components/Pages";
 
 export const PageHome = loadable(
   () =>
-    import(
-      /* webpackChunkName: "page-home" */ `${BASE_PAGES_PATH}/Home/Home.tsx`
-    ),
+    import(/* webpackChunkName: "home" */ `${BASE_PAGES_PATH}/Home/Home.tsx`),
 );
 
 export const PageSearch = loadable(
   () =>
     import(
-      /* webpackChunkName: "page-search" */ `${BASE_PAGES_PATH}/Search/Search.tsx`
+      /* webpackChunkName: "search" */ `${BASE_PAGES_PATH}/Search/Search.tsx`
     ),
 );
 
 export const PageSettings = loadable(
   () =>
     import(
-      /* webpackChunkName: "page-settings" */ `${BASE_PAGES_PATH}/Settings/Settings.tsx`
+      /* webpackChunkName: "settings" */ `${BASE_PAGES_PATH}/Settings/Settings.tsx`
     ),
 );
 
 export const PageNewTrain = loadable(
   () =>
     import(
-      /* webpackChunkName: "page-newtrain" */ `${BASE_PAGES_PATH}/Train/NewTrain/NewTrain.tsx`
+      /* webpackChunkName: "new-train" */ `${BASE_PAGES_PATH}/Train/NewTrain/NewTrain.tsx`
     ),
 );
 
 export const PageViewTrain = loadable(
   () =>
     import(
-      /* webpackChunkName: "page-viewtrain" */ `${BASE_PAGES_PATH}/Train/ViewTrain/ViewTrain.tsx`
+      /* webpackChunkName: "view-train" */ `${BASE_PAGES_PATH}/Train/ViewTrain/ViewTrain.tsx`
     ),
 );
 
 export const PageStartTrain = loadable(
   () =>
     import(
-      /* webpackChunkName: "page-starttrain" */ `${BASE_PAGES_PATH}/Train/StartTrain/StartTrain.tsx`
+      /* webpackChunkName: "start-train" */ `${BASE_PAGES_PATH}/Train/StartTrain/StartTrain.tsx`
     ),
 );
